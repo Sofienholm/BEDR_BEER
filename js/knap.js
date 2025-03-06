4824ee746865aed870338d7356ed50171e2d2cd0
Sofienholm <eaa24snho@students.eaaa.dk>
2025-03-06 16:50:19 +0100

    mm

--- js/knap.js
+++ js/knap.js
@@ -1,14 +1,7 @@
-document.querySelectorAll('.icon').forEach(icon => {
-    const img = document.createElement('img');
-    img.src = icon.getAttribute('data-img');
-    img.alt = icon.getAttribute('data-text');
-    icon.appendChild(img);
+function toggleDropdown() {
+    let box = document.getElementById("beerBox");
+    let button = document.querySelector(".dropdown-btn");
 
-    const popup = document.createElement('div');
-    popup.classList.add('popup');
-    popup.textContent = icon.getAttribute('data-text');
-    icon.appendChild(popup);
-
-    icon.addEventListener('mouseenter', () => popup.style.opacity = '1');
-    icon.addEventListener('mouseleave', () => popup.style.opacity = '0');
-});
+    box.classList.toggle("show");
+    button.classList.toggle("open");
+}
